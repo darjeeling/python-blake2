6f49ce88be630a04088cec85de021e1b012a6edb
KwonHan Bae <darjeeling@gmail.com>
2013-01-02 23:37:33 +0900

    modify test to give key in blake2

--- blake2test/test.py
+++ blake2test/test.py
@@ -8,11 +8,11 @@ tests = Tests()
 @tests.test
 def blake2b():
     """Test blake2s results"""
-    assert blake2.blake2("blake2") == '22ccfc5a19a58358eb07a89e9b2c29356fff5ed0a50364ae1391649fc6152a2a700fc712f1ba031f3ee6e64736feacf49308755eabc9719352b54192b820f435'
-    assert blake2.blake2("hello world") == '1b48ac3711e63c9dadcff921866bf4adabc4b0b204399d582b5d695e67c29a100bade80ec193ae220de7aeb200d7a4069b917d3d4b4e6a230b0dc0a5c8a03c3e'
-    assert blake2.blake2("hello world",hashSize=32) == 'a9f88a40d4d7840b1669c0da696b5db1e545e9b6b606a13d500e0787f2cb375e'
-    assert blake2.blake2("hello world",hashSize=16) == 'a5bd8867c9201df110adf0c20d164f7a'
-    assert blake2.blake2("hello world",hashSize=4) == '72920435'
+    assert blake2.blake2("blake2", key="blake2") == 'b202b477a797e984e6a2b976ca4cb7d3944004b3ef6cde80341c7853a2dd7e2f9e08cda6d7372812cf75e8c7741fb656cec3a119772e2e715cebc76433fafd4d'
+    assert blake2.blake2("hello world", key="hello world") == '45589476ec87eec2e35a20817e822ec894a3c35116dfb8a35b2b966a7193deafbb65684f102a8dee449d75065251e7d8fe71c05b4d2452329caf6c5bb48d66f9'
+    assert blake2.blake2("hello world", hashSize=32, key="hello world") == '177a8d7754e0aaa6645179c6c9933c3c57a4880e91223f56ded5d3e3cd7144dd'
+    assert blake2.blake2("hello world", hashSize=16, key="hello world") == '8fe7d57f5c53d8afd00f552269502b81'
+    assert blake2.blake2("hello world", hashSize=4, key="hello world") == 'bbd7cc6e'
 
 
 if __name__ == '__main__':
