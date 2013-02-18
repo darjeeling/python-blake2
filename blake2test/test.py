68825db292bb213ed0c421fdde64beddc069299e
pgonee <pgonee@gmail.com>
2013-02-19 01:26:27 +0900

    add test case for string containing \x00.

--- blake2test/test.py
+++ blake2test/test.py
@@ -13,6 +13,7 @@ def blake2b():
     assert blake2.blake2("hello world", hashSize=32, key="hello world") == '177a8d7754e0aaa6645179c6c9933c3c57a4880e91223f56ded5d3e3cd7144dd'
     assert blake2.blake2("hello world", hashSize=16, key="hello world") == '8fe7d57f5c53d8afd00f552269502b81'
     assert blake2.blake2("hello world", hashSize=4, key="hello world") == 'bbd7cc6e'
+    assert blake2.blake2("hello\x00world", key="hello\x00world") == 'e06e51bbdc12363243a55ddc23aaeb310faceec72e21d93c85d7e77360aa48cb6baf2963661bf857b1686c89f5dd209f0abee10aa2e38d0318043718976bcb60'
     assert blake2.blake2(None) == None
 
 
@@ -23,6 +24,7 @@ def blake2s():
     assert blake2.blake2s("hello world", key="hello world") == '846d7f4e70f94df2b07e2f5d59d271d5b4627ab64cc0fc376f411448528bee49'
     assert blake2.blake2s("hello world", hashSize=16, key="hello world") == '4e989fc7739d052dd93ec88962137c08'
     assert blake2.blake2s("hello world", hashSize=4, key="hello world") == 'fef7f902'
+    assert blake2.blake2s("hello\x00world", key="hello\x00world") == '36429945e82aec7853fd2bd1c7349a65e4457db81c059b287f7a859e3b26e3f4'
     assert blake2.blake2(None) == None
 
 
